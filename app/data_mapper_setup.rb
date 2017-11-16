deff5729479adbd63bb764315dd5d10401a58c56
Lewis <lewisyoul@gmail.com>
2017-11-16 09:42:48 +0000

    remove extra data_mapper_setup + create links + add placeholders

--- app/data_mapper_setup.rb
+++
@@ -1,9 +0,0 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-require 'dm-migrations'
-
-
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
